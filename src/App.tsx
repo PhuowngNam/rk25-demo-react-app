2ab030ac350f836c91e1790e796c7ab9c2e8ed39
PhuowngNam <npnam0706@gmail.com>
2022-10-06 23:33:11 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Person, PersonF, PersonProps} from "./component/class-component/Person";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const userList: PersonProps[] = [
+        {fullname: "Pham Quang Dung", age: 23, height: '180cm'},
+        {fullname: "Pham Quang Dung", age: 23},
+    ]
+    console.log(123)
+    return (
+        <div className="App">
+            {/*<Person fullname={"Nguyen Van Hung"} age={22} height={'180cm'}/>*/}
+            {/*<Person fullname={"Pham Quang Dung"} age={23}/>*/}
+
+            {/*{userList.map(user => {*/}
+            {/*    return (*/}
+            {/*        <PersonF aside={<Aside/>} fullname={user.fullname} age={user.age} height={user.height}/>*/}
+            {/*    )*/}
+            {/*})}*/}
+
+            {userList.map(user => {
+                return (
+                    <Person fullname={user.fullname} age={user.age} height={user.height}/>
+                )
+            })}
+        </div>
+    );
 }
+
+const Aside = () => {
+    return (
+        <div>
+            Danh sách
+        </div>
+    )
+}
+
 
 export default App;
---
+++ src/component/class-component/Person.tsx
@@ -0,0 +1,67 @@
+import React, {Component, FunctionComponent} from 'react';
+
+export type PersonProps = {
+    fullname: string,
+    age: number,
+    height?: string,
+    aside?: any
+}
+
+export class Person extends  React.Component<PersonProps, any> {
+
+    static defaultHeight: number = 0;
+
+    constructor(props: PersonProps) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+    }
+
+    handleClickIncrease = () => {
+
+        this.setState((prevState: any) => {
+            return {
+                counter: prevState.counter + 1
+            }
+        })
+    }
+
+    handleClickDecrease = () => {
+        const newCounter = this.state.counter - 1;
+
+        this.setState({
+            counter: newCounter
+        })
+    }
+
+    render() {
+        console.log("render")
+        return (
+            <div>
+                <h3>Fullname: {this.props.fullname}</h3>
+                <p>Age: {this.props.age}</p>
+                <p>Height: {this.props.height ? this.props.height : Person.defaultHeight}</p>
+
+                <div>STATE COUNTER: {this.state.counter}</div>
+                <button onClick={this.handleClickIncrease}>Increase</button>
+                <button onClick={this.handleClickDecrease}>Decrease</button>
+            </div>
+        )
+    }
+}
+
+export const PersonF = (props: PersonProps) => {
+    const defaultHeight: number = 0;
+
+    return (
+        <div>
+            {props.aside}
+            <div>
+                <h3>Fullname: {props.fullname}</h3>
+                <p>Age: {props.age}</p>
+                <p>Height: {props.height ? props.height : defaultHeight}</p>
+            </div>
+        </div>
+    )
+}
